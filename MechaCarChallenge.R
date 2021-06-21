b1105245b42099c8f4b5cc43222c3db4a977ceda
Andrew <drewski1259@gmail.com>
2021-06-20 20:29:26 -0500

    add R file and resources folder

---
+++ MechaCarChallenge.R
@@ -0,0 +1,18 @@
+#load dplyr package
+library(dplyr)
+#read csv
+mpg_table <- read.csv(file='MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
+#check linear model syntax
+lm(vehicle_length ~ mpg,mpg_table)
+#perform linear regression on all columns against dataframe
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_table)
+#perform summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_table))
+#load tidyverse
+library(tidyverse)
+# convert suspension_coil.csv to dataframe
+coil_table<- read.csv(file='Suspension_Coil.csv', check.names=F,stringsAsFactors=F)
+#summarize PSI column for mean, median, variance, and standard deviation
+total_summary <- coil_table %>% summarize(mean_PSI = mean(PSI), median_PSI = median(PSI), variance_PSI = var(PSI), STDEV_PSI = sd(PSI, na.rm =FALSE))
+#group by PSI and summarize PSI column for mean, median, variance, and standard deviation
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot)%>% summarize(mean_PSI = mean(PSI), median_PSI = median(PSI), variance_PSI = var(PSI), STDEV_PSI = sd(PSI, na.rm =FALSE))
